a364c89b601cf4bdbea0a2f1e1b3bf331a846d25
Hssain Aitkadir <haitkadi@e3r2p11.1337.ma>
2022-06-08 00:04:51 +0100

    Add: check operators syntax function add in lexer part but it dosn't completed yet

--- srcs/lexer/srcs/lexer.c
+++ srcs/lexer/srcs/lexer.c
@@ -1,5 +1,24 @@
 #include "../lexer.h"
 
+
+// static char check_operators(char *line)
+// {
+// 	int i;
+// 	char flag;
+
+// 	i = 0;
+// 	flag = 0;
+// 	while(line[i])
+// 	{
+// 		if (line[i] == '<' && !ft_isalnum(line[i + 1]))
+// 			flag = !flag;
+		
+// 		i++;
+// 	}
+// 	return (0);
+// }
+
+
 char lexer(char *line)
 {
 	if (check_qoutes(line))
