5fc7c8fce387e935f1aa1f335462897041ce5926
Hssain Aitkadir <haitkadi@e3r3p11.1337.ma>
2022-06-24 14:04:50 +0100

    Update: prototype of get_cmd function  modefied

--- srcs/parser/parser.h
+++ srcs/parser/parser.h
@@ -7,7 +7,7 @@ void	free_path(char **path);
 char	**get_path(t_env *env);
 char	*check_cmd(t_env *env, char *cmd);
 char	**get_switchs( t_token **token);
-char	*get_cmd(t_shell **shell, t_env *env, t_token **token);
+void	get_cmd(t_shell **shell, t_env *env, t_token **token);
 
 char	is_operator(t_token *token);
 char	process_data(t_shell **shell, t_token *token, t_env *env);
