21f645174dbbc634cf895ab19a16a8828cff0300
Hssain Aitkadir <haitkadi@e3r4p9.1337.ma>
2022-07-01 22:24:23 +0100

    New: compearing with lower case

--- libft/libft.h
+++ libft/libft.h
@@ -59,6 +59,7 @@ int		ft_isprint(int c);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(char *s1, char *s2);
+int		ft_strcmp_tl(char *s1, char *s2);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
 int		ft_lstsize(t_list *lst);
