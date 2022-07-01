64023a4e8fc5c644c5e0db31e5267d4f5a871942
Hssain Aitkadir <haitkadi@e3r4p9.1337.ma>
2022-07-01 22:23:45 +0100

    Update: protection

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -9,6 +9,7 @@
 /*   Updated: 2021/07/08 19:08:04 by haitkadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -16,12 +17,12 @@ int	ft_strcmp(char *s1, char *s2)
 	unsigned char	*us2;
 	int				i;
 
+	if (!s1 && !s2)
+		return (0);
 	us1 = (unsigned char *) s1;
 	us2 = (unsigned char *) s2;
 	i = 0;
 	while (us1[i] && us1[i] == us2[i])
-	{
 		i++;
-	}
 	return (us1[i] - us2[i]);
 }
