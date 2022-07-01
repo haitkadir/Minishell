37a0efd0b0c3311351ff0c354a5ea090b5a75dc6
Hssain Aitkadir <haitkadi@e3r4p9.1337.ma>
2022-07-01 22:25:33 +0100

    Change: utils file created outside of executer part

---
+++ srcs/utils/utils.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils2.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sahafid <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/30 16:06:58 by sahafid           #+#    #+#             */
+/*   Updated: 2022/05/30 16:07:00 by sahafid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+
+int	check_builtins(char *str)
+{
+	if (!ft_strcmp_tl(str, "pwd"))
+		return (1);
+	else if (!ft_strcmp(str, "export"))
+		return (1);
+	else if (!ft_strcmp(str, "unset"))
+		return (1);
+	else if (!ft_strcmp_tl(str, "env"))
+		return (1);
+	else if (!ft_strcmp(str, "exit"))
+		return (1);
+	else if (!ft_strcmp_tl(str, "cd"))
+		return (1);
+	else if (!ft_strcmp_tl(str, "echo"))
+		return (1);
+	return (0);
+}
+
+void	put_error(char *keyword, char *msg, int err)
+{
+	ft_putstr_fd("\033[4;31mMinishell\033[0m: ", 2);
+	ft_putstr_fd(keyword, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(msg, 2);
+	ft_putstr_fd("\n", 2);
+	status.exit_status = err;
+}
